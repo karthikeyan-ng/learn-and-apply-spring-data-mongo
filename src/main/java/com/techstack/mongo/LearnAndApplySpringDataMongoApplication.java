9aa83771c3e48fc1aa600de77ad87c345adf5451
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-21 18:21:26 +0530

    Initial Commit. Application created and extracted from start.spring.io

---
+++ src/main/java/com/techstack/mongo/LearnAndApplySpringDataMongoApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndApplySpringDataMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAndApplySpringDataMongoApplication.class, args);
+	}
+
+}
