b5f0dc479cb4bea40d78218a36be8293ebec339d
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-21 23:49:13 +0530

    Fetch all the document from the MongoDB collection

--- Readme.md
+++ Readme.md
@@ -33,3 +33,29 @@ Use `POST` `legostore/api` to insert json to MongoDB
 	"nbParts": 454
 }
 ```
+
+## Fetch All
+Use `GET` `/all` to get all the values from MongoDB Collection
+```json
+[
+    {
+        "id": "5e9f369cda730013c3acb0e0",
+        "name": "NASA Apollo Saturn V",
+        "difficulty": "MEDIUM",
+        "theme": "NASA",
+        "reviews": [
+            {
+                "userName": "Joe",
+                "rating": 9
+            }
+        ],
+        "deliveryInfo": {
+            "deliveryDate": "2020-04-21",
+            "deliveryFee": 50,
+            "inStock": true
+        },
+        "nbParts": 0
+    }
+]
+```
+
--- src/main/java/com/techstack/mongo/controller/LegoStoreController.java
+++ src/main/java/com/techstack/mongo/controller/LegoStoreController.java
@@ -3,10 +3,13 @@ package com.techstack.mongo.controller;
 import com.techstack.mongo.model.LegoSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +21,11 @@ public class LegoStoreController {
     @PostMapping
     public void insert(@RequestBody LegoSet legoSet) {
         this.mongoTemplate.insert(legoSet);
+    }
+
+    @GetMapping("/all")
+    public List<LegoSet> getAll() {
+        return this.mongoTemplate.findAll(LegoSet.class);
     }
 
 }
